a0f0369d188b3eeb3f9893d20336e81e3552412a
jtavara23 <joshuatavara23@gmail.com>
2016-06-25 06:34:32 -0300

    generacion descriptor terminada

--- ffttest.m
+++ ffttest.m
@@ -1,9 +1,11 @@
-nameImg=strcat('.\Img\14.jpg');
+nameImg=strcat('..\Img\2.jpg');
 img_RGB = imread(nameImg);
 img_gray=rgb2gray(img_RGB);
-figure, imshow(img_gray,[]);
+img = imresize(img_gray, [400 400]);
 
-F= fft2(img_gray);
+% figure, imshow(img,[]);
+
+F= fft2(img);
 
 F2 = fftshift(F);%swap (Top-Left with Bottom-Right) (Top-Right with Bottom-Left)
 
@@ -15,4 +17,21 @@ S2 = log(1+S);%para mejorar el rango dinamico de visualizacao
 %so we can better see the transform.
 % +1 since log(0) is undefined
 figure, imshow(S2,[]);
-disp(S2);
+[Heigth,Width] = size(S2);%heightxwidth
+%disp(S2);
+cooX = Width/2;
+cooY = Heigth/2;
+descriptore = [];
+k=1;
+range = 10;
+for i=cooY-range: cooY+range-1
+    for j=cooX-range: cooX+range-1
+      disp(i);
+      disp(j);
+       descriptore(k) = S2(i,j);
+       k = k+1;
+    end
+
+end
+% disp(descriptore);
+disp(size(descriptore));
